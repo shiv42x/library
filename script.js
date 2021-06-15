e29abc48a2b777a33ce538f977d930e30adf979e
Shivam Patel <shivam2.patel@ryerson.ca>
2021-06-15 18:37:27 -0400

    added modal form

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 let myLibrary = []; 
 const newBookBtn = document.getElementById("btn-new-book");
+const modal = document.getElementById("div-new-book");
+const span = document.getElementsByClassName("close")[0];
 
-newBookBtn.addEventListener("click", () => {
-    alert("you clicked!");
-})
+document.getElementById("form-new-book").onsubmit = function() {
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    var hasRead = false; // False by default as "no" radio button checked by default
+    if (document.querySelector("input[name=\"read\"]:checked").value == "yes") {
+        hasRead = true;
+    }
+    const newBook = new Book(title, author, pages, hasRead);
+    displayBook(newBook);
+    modal.style.display = "none";
+    return false;
+}
+
+newBookBtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 /* -- TESTER START -- */
-
 const theHobbit = new Book("The Hobbit", "J. R. R. Tolkien", 295, true); 
 const theHobbit2 = new Book("The Hobbit2", "J. R. R. Tolkien", 295, true);
 
@@ -17,7 +42,6 @@ addBookToLibrary(theHobbit2);
 addBookToLibrary(theHobbit2);
 
 myLibrary.forEach(book => displayBook(book));
-
 /* -- TESTER END -- */
 
 function Book(title, author, numPages, read) { 
@@ -35,11 +59,6 @@ function Book(title, author, numPages, read) {
     }
 }
 
-function addBook() {
-    
-}
-
-
 function addBookToLibrary(book) { 
     myLibrary.push(book); 
 }
@@ -52,8 +71,17 @@ function displayBook(book) {
     const bookTitle = document.createElement("h4");
     const bookAuthor = document.createElement("p");
     const bookPages = document.createElement("p");
+    const hasRead = document.createElement("p");
     container.style.textAlign = "center";
-    
+
+    // If cnd for whether read or not
+    if (book.read) {
+        hasRead.textContent = "You have read this book.";
+    }
+    else {
+        hasRead.textContent = "Unread";
+    }
+
     // Add class card to apply CSS
     bookCard.className = "card";
     
@@ -66,6 +94,7 @@ function displayBook(book) {
     container.appendChild(bookTitle);
     container.appendChild(bookAuthor);
     container.appendChild(bookPages);
+    container.appendChild(hasRead);
     bookCard.appendChild(container);
     display.appendChild(bookCard);
 }
